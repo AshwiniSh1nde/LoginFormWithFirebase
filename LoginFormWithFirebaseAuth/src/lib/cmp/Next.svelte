0137184e0bab40b2978ae182b38c43e3519726ff
Ashwini Shinde <113597549+AshwiniSh1nde@users.noreply.github.com>
2023-02-05 13:24:10 +0530

    initial commit

---
+++ LoginFormWithFirebaseAuth/src/lib/cmp/Next.svelte
@@ -0,0 +1,3 @@
+
+
+<h1>Next Page</h1>
---
+++ LoginFormWithFirebaseAuth/src/routes/+page.svelte
@@ -0,0 +1,34 @@
+<script>
+    import Next from "../lib/cmp/Next.svelte";
+    import Login from "../lib/cmp/Login.svelte";
+    import { initializeApp } from "firebase/app";
+    import { getAuth } from "firebase/auth";
+
+//---------------Write your firebase configuration here --------------
+    const firebaseConfig = {
+    apiKey: "your-api-key",
+    authDomain: "your-project-id.firebaseapp.com",
+    projectId: "your-project-id",
+    storageBucket: "your-project-id.appspot.com",
+    messagingSenderId: "your-sender-id",
+    appId: "your-app-id"
+};
+
+    // Initialize Firebase
+    const app = initializeApp(firebaseConfig);
+    // Initialize Firebase Authentication and get a reference to the service
+    const auth = getAuth(app);
+    
+    var loggedIn = false; // assume the user is not logged in
+
+    function handleLogin() {
+        loggedIn = true;
+    }
+
+</script>
+
+{#if !loggedIn}
+    <Login {handleLogin} />
+{:else}
+    <Next />
+{/if}
